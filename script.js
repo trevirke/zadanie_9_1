61b470335b5e3422cd0b2afb05c125b0cff89eb5
Sylwia Mamczur <sylwia.mamczur@gmail.com>
2017-11-29 21:10:02 +0100

    Add index.html

--- script.js
+++ script.js
@@ -15,15 +15,3 @@ var triangle1Area = getTriangleArea(9, 4);
 var triangle2Area = getTriangleArea(2, 10);
 var triangle3Area = getTriangleArea(3, 5);
 
-//Sprawdzam, czy działa :) 
-function getTriangleArea(a, h) {
-  if (a, h <= 0) {
-    console.log('Nieprawidlowe dane');
-  }
-  else { 
-    return a*h/2;
-  }
-}
-console.log(triangle2Area);
-
-
