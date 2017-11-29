fbdc85f6cc9a000acfb10fb2f4b142d663ee4df1
Sylwia Mamczur <sylwia.mamczur@gmail.com>
2017-11-29 17:52:09 +0100

    Add js

---
+++ script.js
@@ -0,0 +1,29 @@
+function getTriangleArea(a, h) {
+  if (a, h <= 0) {
+    console.log('Nieprawidlowe dane');
+  }
+  else { 
+    return a*h/2;
+  }
+}
+console.log(getTriangleArea(10, 6));
+
+
+
+//Zmienne przechowujące pole trójkąta
+var triangle1Area = getTriangleArea(9, 4);
+var triangle2Area = getTriangleArea(2, 10);
+var triangle3Area = getTriangleArea(3, 5);
+
+//Sprawdzam, czy działa :) 
+function getTriangleArea(a, h) {
+  if (a, h <= 0) {
+    console.log('Nieprawidlowe dane');
+  }
+  else { 
+    return a*h/2;
+  }
+}
+console.log(triangle2Area);
+
+
